9a017b284517a021a56368108949c30d6daae3e7
brianwu <fslikeyou1033@yahoo.com>
2025-04-25 01:01:13 -0400

    add django to compose file / add entrypoint script

---
+++ server/entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# Run migrations and collect static (optional)
+python manage.py migrate --noinput
+python manage.py collectstatic --noinput
+
+# Start server
+exec gunicorn server.wsgi:application --bind 0.0.0.0:${PORT:-8000}
