7fa021cfcd408aa48bd6affca2508b449248aa9b
brianwu <fslikeyou1033@yahoo.com>
2025-04-26 04:06:40 -0400

    remove static file collection

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -2,7 +2,7 @@
 
 # Run migrations and collect static (optional)
 python manage.py migrate --noinput
-python manage.py collectstatic --noinput
+# python manage.py collectstatic --noinput
 
 # Start server
 exec gunicorn server.wsgi:application --bind 0.0.0.0:${PORT:-8000}
