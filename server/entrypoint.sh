6f65d191267f247e80d38407cb51de82a0341f5c
brianwu <fslikeyou1033@yahoo.com>
2025-05-16 00:51:19 -0400

    add daphne server

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -5,4 +5,5 @@ python manage.py migrate --noinput
 # python manage.py collectstatic --noinput
 
 # Start server
-exec gunicorn server.wsgi:application --bind 0.0.0.0:${PORT:-8000}
+# exec gunicorn server.wsgi:application --bind 0.0.0.0:${PORT:-8000}
+exec daphne -b 0.0.0.0 -p 8000 server.asgi:application
